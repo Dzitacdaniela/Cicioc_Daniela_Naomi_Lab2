004b33f76e348fe8adea37edeb190c24df81c082
Dzitacdaniela <77731209+Dzitacdaniela@users.noreply.github.com>
2023-11-09 10:39:29 +0100

    Add project files.

---
+++ Data/Cicioc_Daniela_Naomi_Lab2Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Cicioc_Daniela_Naomi_Lab2.Models;
+
+namespace Cicioc_Daniela_Naomi_Lab2.Data
+{
+    public class Cicioc_Daniela_Naomi_Lab2Context : DbContext
+    {
+        public Cicioc_Daniela_Naomi_Lab2Context (DbContextOptions<Cicioc_Daniela_Naomi_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Cicioc_Daniela_Naomi_Lab2.Models.Book> Book { get; set; } = default!;
+
+        public DbSet<Cicioc_Daniela_Naomi_Lab2.Models.Publisher>? Publisher { get; set; }
+    }
+}
---
+++ Pages/Publishers/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Cicioc_Daniela_Naomi_Lab2.Data;
+using Cicioc_Daniela_Naomi_Lab2.Models;
+
+namespace Cicioc_Daniela_Naomi_Lab2.Pages.Publishers
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Cicioc_Daniela_Naomi_Lab2.Data.Cicioc_Daniela_Naomi_Lab2Context _context;
+
+        public IndexModel(Cicioc_Daniela_Naomi_Lab2.Data.Cicioc_Daniela_Naomi_Lab2Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Publisher> Publisher { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Publisher != null)
+            {
+                Publisher = await _context.Publisher.ToListAsync();
+            }
+        }
+    }
+}
